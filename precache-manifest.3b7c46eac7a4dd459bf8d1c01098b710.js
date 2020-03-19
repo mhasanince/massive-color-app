65b4e9b610af7f29c9c9a98768c989145a5c0c57
mhasanince <mhasanince@gmail.com>
2020-03-19 16:32:00 +0300

    Updates

---
+++ precache-manifest.3b7c46eac7a4dd459bf8d1c01098b710.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb4f0701f06f5ddd48631f1bfe2c49a",
+    "url": "/massive-color-app/index.html"
+  },
+  {
+    "revision": "8d79bae385de508a7c46",
+    "url": "/massive-color-app/static/css/2.77b93774.chunk.css"
+  },
+  {
+    "revision": "f78c6af900576be5a772",
+    "url": "/massive-color-app/static/css/main.83293db6.chunk.css"
+  },
+  {
+    "revision": "8d79bae385de508a7c46",
+    "url": "/massive-color-app/static/js/2.d89c2d79.chunk.js"
+  },
+  {
+    "revision": "b9d733f5c3059f97623ffaa7f2390db4",
+    "url": "/massive-color-app/static/js/2.d89c2d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78c6af900576be5a772",
+    "url": "/massive-color-app/static/js/main.ee573e08.chunk.js"
+  },
+  {
+    "revision": "691b4fc2d3c24c1b2c28",
+    "url": "/massive-color-app/static/js/runtime-main.511a658d.js"
+  }
+]);
